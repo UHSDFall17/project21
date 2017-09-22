ed65c369e6b7acf7e9869b0783d289078d66422f
gus07ven <gustavoarismendi@hotmail.com>
2017-09-22 13:39:34 -0500

    Upload perassis maven structure

---
+++ project21/README.md
@@ -0,0 +1,2 @@
+# project21
+This is repository for the project assigned to Group 21
---
+++ project21/perassis/src/main/java/com/perassis/org/App.java
@@ -0,0 +1,13 @@
+package com.perassis.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Perassis World!" );
+    }
+}
