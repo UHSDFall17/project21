13b917fd91c0901ec6b0f74c304cfa3976791a0b
Rolando <rjr0113@gmail.com>
2017-09-28 22:11:21 -0500

    user class

--- project21/perassis/src/main/java/com/perassis/org/User.java
+++ project21/perassis/src/main/java/com/perassis/org/User.java
@@ -2,5 +2,43 @@ package com.perassis.org;
 
 public class User {
 
-    // TODO: Write User class
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+//    public User(String newUserName, String newPassword){
+//        this.userName = newUserName;
+//        this.password = newPassword;
+//    }
+//    public User(String newFirstName, String newLastName, String newUserName, String newPassword){
+//        this.firstName = newFirstName;
+//        this.lastName = newLastName;
+//        this.userName = newUserName;
+//        this.password = newPassword;
+//    }
+    public void setFirstName(String newFirstName){
+        this.firstName = newFirstName;
+    }
+    public void setLastName(String newLastName){
+        this.lastName = newLastName;
+    }
+    public void setUserName(String newUserName){
+        this.userName = newUserName;
+    }
+    public void setPassword(String newPassword){
+        this.password = newPassword;
+    }
 }
