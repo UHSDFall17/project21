3b9d81faee62bbab9a046cebdf0cd93ac6bdf814
senthilkumarsivaraman <31826582+senthilkumarsivaraman@users.noreply.github.com>
2017-10-03 18:18:15 -0500

    Tasklist Function

--- project21/perassis/src/main/java/com/perassis/org/Task.java
+++ project21/perassis/src/main/java/com/perassis/org/Task.java
@@ -1,215 +1,77 @@
 package com.perassis.org;
 
-import java.util.Scanner;
+import java.util.Date;
 
 public class Task
 {
+    private int taskID;
+    private String title;
+    private Date dateTime;
+    private String notes;
+    private UserCalendar userCalendar;
 
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1.Add your Remainder \n 2.Modify your Remainder \n 3. Delete your Remainder");
-		int task = scanner.nextInt();
-
-
-	   if(task==1)
-
-	     {
-		      Add();
-	     }
-	
-       else if (task ==2)
-	 
-	     {
-       		  Modify();
-       	 }
-
- 	   else
-	     {
- 		  	  Delete();
-	     }
-    
-	}
+    public Task(String title, Date dateTime, String notes, User user) {
+        this.title = title;
+        this.dateTime = dateTime;
+        this.notes = notes;
+        this.user = user;
+    }
 
 
-	
+    public String getTitle()
 
+    {
+        return title;
+    }
 
-	Add()
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
 
-	 {
-		System.out.println("For which day you want to add the task \n 1.Today \n2.Tomorrow \n3.Some day");
-		int day = scanner.nextInt();
-		   
-		switch(day)
-		{
-		
-       			 case '1' :
-       				 Today();
-        			 break;
-        
-      			  case '2' :
-      				  Tomorrow();
-      				  break;
-			
-        		case '3' :
-        			Someday();
-        			break;
-			
-        		default :
-           			System.out.println("Invalid grade");
-       	 }
-		
-	 }
-		
-		
-		
-		Today()
-		{
-			System.out.println("1.Call \n 2.Check\n 3.Wish \n 4.Email \n 5.Buy \n 6.Meet/Schedule \n 7.Clean \n 8.Pay \n 9.Study \n 10.Other");
-			/*Jump to the Specific Function*/
-		}
-		
-		
-		Tomorrow()
-		{
-			System.out.println("1.Call \n 2.Check\n 3.Wish \n 4.Email \n 5.Buy \n 6.Meet/Schedule \n 7.Clean \n 8.Pay \n 9.Study \n 10.Other");
-			/*Jump to the Specific Function*/
-		}
-		
-		Someday()
-		{
-			/* Get the date */
-			System.out.println("1.Call \n 2.Check\n 3.Wish \n 4.Email \n 5.Buy \n 6.Meet/Schedule \n 7.Clean \n 8.Pay \n 9.Study \n 10.Other");
-			/*Jump to the Specific Function*/
-		}
-		
-		
-		
-		
-		Call()
-		{
-			/*Name and Number, Date/Time*/
-		}
-		
-		Check()
-		{
-			/*Check for String, Date/Time*/
-		}
-		
-		Wish()
-		{
-			/*Wish name, Date/Time*/
-		}
-		
-		Email()
-		{
-			/*Email-id, Date/Time*/
-		}
-		
-		Buy()
-		{
-			/*Buy Product, string, Date/Time */
-		}
-		
-		Meet()
-		{
-			/*Person name, Date/Time*/
-		}
-		
-		Clean()
-		{
-			/*clean the item, Date/Time*/
-		}
-		
-		Pay()
-		{
-			/*Pay for Name, Date/Time*/
-		}
-		
-		Study()
-		{
-			/*Study String, Date/Time*/
-		}
-		
-		Other()
-		{
-			/*Any String, Date/Time*/
-		}
+    public Date getDateTime()
+    {
+        return dateTime;
+    }
 
+    public void setDateTime(Date dateTime)
+    {
+        this.dateTime = dateTime;
+    }
 
+    public String getNotes()
+    {
+        return notes;
+    }
 
-	    }
+    public void setNotes(String notes)
+    {
+        this.notes = notes;
+    }
 
+    public User getUser()
+    {
+        return user;
+    }
 
+    public void setUser(User user)
+    {
+        this.user = user;
+    }
 
+    public void addTask(User user)
+    {
 
+    }
 
-	Modify()
-	  
-	    {
-		System.out.println("For which day you want to modify the task \n 1.Today \n2.Tomorrow \n3.Some day");
-		int day1 = scanner.nextInt();
+    public void editTask()
+    {
+    }
 
+    public void deleteTask()
+    {
 
-		switch(day)
-		{
-		
-       			 case '1' :
-
-				/*List down all the tasks
-				Get the Specific Task number and Display it
-				Get the modified task and replace the old one with this in the database*/
-        			break;
-        
-      			  case '2' :
-        			break;
-			
-        		case '3' :
-        			break;
-			
-        		default :
-           			System.out.println("Invalid grade");
-       		 }
-	    }
-
-
-
-	
-	delete()
-	  
-	    {
-		System.out.println("For which day you want to delete the task \n 1.Today \n2.Tomorrow \n3.Some day");
-		int day1 = scanner.nextInt();
-
-
-		switch(day)
-		{
-		
-       			 case '1' :
-
-       				 /*List down all the tasks
-       				 Get the Specific Task number and Display it
-       				 Delete the task from the database*/
-        			break;
-        
-      			  case '2' :
-        			break;
-			
-        		case '3' :
-        			break;
-			
-        		default :
-           			System.out.println("Invalid grade");
-       		
-	 }
-
-
+    }
 }
 
-
-
-
-		
-
-		
 
