ef7573673f63fe1eead7e9062148071a7d36f9ba
Rolando <rjr0113@gmail.com>
2017-10-07 12:43:35 -0500

    database

--- project21/perassis/src/main/java/com/perassis/org/User.java
+++ project21/perassis/src/main/java/com/perassis/org/User.java
@@ -7,38 +7,31 @@ public class User {
     private String userName;
     private String password;
 
-    public String getFirstName(){
+    public String getFirstName() {
         return firstName;
     }
-    public String getLastName(){
+
+    public String getLastName() {
         return lastName;
     }
-    public String getUserName(){
+
+    public String getUserName() {
         return userName;
     }
-    public String getPassword(){
+
+    public String getPassword() {
         return password;
     }
-//    public User(String newUserName, String newPassword){
-//        this.userName = newUserName;
-//        this.password = newPassword;
-//    }
-//    public User(String newFirstName, String newLastName, String newUserName, String newPassword){
-//        this.firstName = newFirstName;
-//        this.lastName = newLastName;
-//        this.userName = newUserName;
-//        this.password = newPassword;
-//    }
-    public void setFirstName(String newFirstName){
-        this.firstName = newFirstName;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
     }
-    public void setLastName(String newLastName){
-        this.lastName = newLastName;
-    }
-    public void setUserName(String newUserName){
-        this.userName = newUserName;
-    }
-    public void setPassword(String newPassword){
-        this.password = newPassword;
+
+    public User(String firstName, String lastName, String userName, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
     }
 }
---
+++ project21/perassis/src/test/java/com/perassis/org/UserTest.java
@@ -0,0 +1,22 @@
+package com.perassis.org;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+    @org.junit.Test
+    public void getFirstName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getLastName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getUserName() throws Exception {
+    }
+
+    @org.junit.Test
+    public void getPassword() throws Exception {
+    }
+
+}
