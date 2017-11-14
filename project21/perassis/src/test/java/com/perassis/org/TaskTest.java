800989ce598a0f54d772f8dccd98a551ef7a94a7
senthilkumarsivaraman <31826582+senthilkumarsivaraman@users.noreply.github.com>
2017-11-14 08:38:51 -0600

    Moving Task Test Case

--- project21/perassis/src/test/java/com/perassis/org/TaskTest.java
+++ project21/perassis/src/test/java/com/perassis/org/TaskTest.java
@@ -8,7 +8,24 @@ import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class TaskTest {
+public class TaskTest
+{
+    @Test
+    public void task_setting_the_values() throws Exception
+    {
+        //Arrange
+        Task task;
+        User user1 = new User("Senthilkumar","12345");
+
+        //Act
+        task = new Task("My first task", LocalDate.of(2017,11,12),"Don't forget", user1);
+
+
+        //Assert
+        Assert.assertEquals(task.getDateTime(), LocalDate.of(2017,11,12));
+        Assert.assertEquals(task.getNotes(), "Don't forget");
+        Assert.assertEquals(task.getTitle(), "My first task");
+    }
 
 
 
