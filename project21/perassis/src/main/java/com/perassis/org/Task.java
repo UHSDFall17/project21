091282e6ca0323d913a8d78595235678655a4736
senthilkumarsivaraman <31826582+senthilkumarsivaraman@users.noreply.github.com>
2017-11-28 10:22:08 -0600

    Commiting the changes

--- project21/perassis/src/main/java/com/perassis/org/Task.java
+++ project21/perassis/src/main/java/com/perassis/org/Task.java
@@ -1,18 +1,26 @@
 package com.perassis.org;
 
-import java.time.LocalDate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 public class Task
 {
     private String title;
-    private LocalDate dateTime;
-    private String notes;
+    private String description;
+    private String date;
+    private int hour;
+    private int minute;
     private User user;
+    private Scanner scan;
+    private String time;
 
-    public Task(String title, LocalDate dateTime, String notes, User user) {
+    Task(String title, String description, String date, User user)
+    {
         this.title = title;
-        this.dateTime = dateTime;
-        this.notes = notes;
+        this.description = description;
+        this.date = date;
         this.user = user;
     }
 
@@ -20,6 +28,9 @@ public class Task
     public String getTitle()
 
     {
+        System.out.println("Enter the title of the task");
+        String tempTitle = scan.nextLine();
+        title = "I want to" + tempTitle;
         return title;
     }
 
@@ -28,25 +39,99 @@ public class Task
         this.title = title;
     }
 
-    public LocalDate getDateTime()
+    public String getDescription()
+
     {
-        return dateTime;
+        System.out.println("Enter the Task Description");
+        String tempDescription = scan.nextLine();
+        description = tempDescription;
+        return description;
     }
 
-    public void setDateTime(LocalDate dateTime)
+    public void setDescription(String description)
     {
-        this.dateTime = dateTime;
+        this.description = description;
     }
 
-    public String getNotes()
+
+    public String getDate()
     {
-        return notes;
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Example: 12-25-2017");
+        System.out.print("Enter date: ");
+        String str = sc.nextLine();
+
+        try
+        {
+            Date date = sdf.parse(str);
+
+            sdf = new SimpleDateFormat("EEE, d MMM yyyy");
+            System.out.println("Date: " + sdf.format(date));
+        }
+        catch (ParseException e)
+        {
+
+            System.out.println("Parse Exception - Invaid Input");
+        }
+        return date;
+
     }
 
-    public void setNotes(String notes)
+    public void setDate()
     {
-        this.notes = notes;
+
+        this.date = date;
     }
+
+    public int getHour()
+    {
+        System.out.print("Enter the Hour (0-23) ");
+        hour = scan.nextInt();
+        while(hour < 0 || hour > 23)
+        {
+            System.out.print("Invalid Input, Kindly enter between (0-23)");
+            hour = scan.nextInt();
+        }
+        return hour;
+    }
+
+    public int hour()
+    {
+        return hour;
+    }
+
+    public int getMinute()
+    {
+        System.out.print("Enter the Minute (0-59): ");
+        minute = scan.nextInt();
+        while(minute < 0 || minute > 59)
+        {
+            System.out.print("Invalid Input, Kindly enter between(0-23): ");
+            minute = scan.nextInt();
+        }
+        return minute;
+    }
+
+    public int minute()
+    {
+        return minute;
+    }
+
+
+
+
+    public void setTime()
+    {
+        System.out.println("Enter the time task has to be done");
+        int temp1 = getHour();
+        int temp2 = getMinute();
+        String time = temp1+":"+ temp2;
+        this.time=time;
+    }
+
+
 
     public User getUser()
     {
